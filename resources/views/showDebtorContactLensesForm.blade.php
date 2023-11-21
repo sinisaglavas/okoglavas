d72527f3dc1d7998f25e81d4de96a9c38dc74e92
sinisaglavas <sinisaglavas@ymail.com>
2023-11-21 14:00:09 +0100

    poboljsanje duznik forme

--- resources/views/showDebtorContactLensesForm.blade.php
+++ resources/views/showDebtorContactLensesForm.blade.php
@@ -11,7 +11,7 @@
             </div>
             <div class="col-1"></div>
             <div class="col-8">
-                <h2>Odaberi klijenta:</h2></div>
+                <h2>Novi dužnik:</h2></div>
         </div>
         <div class="row">
             <div class="col-4"></div>
@@ -21,14 +21,14 @@
                     @csrf
                     <label for="find_client">Pronađi klijenta</label>
                     <select name="client" class="form-control" id="find_client">
-                        <option value="0" id="control">Izaberi klijenta</option>
-
+                        <option value="0" id="control">Klikni ovde</option>
                         @foreach($all_clients as $single_client)
                             <option value="{{ $single_client->id}}" class="client">{{ $single_client->name }}</option>
                         @endforeach
                     </select><br>
-                    <label for="debit">Dug</label>
-                    <input type="number" name="debit" class="form-control" id="debit" disabled required><br>
+                    <label for="debit">Iznos duga</label>
+                    <input type="number" name="debit" class="form-control" id="debit"
+                           placeholder="Upis duga je moguć kada odaberete klijenta" disabled required><br>
                     <button class="btn btn-danger form-control">Zapamti</button>
                 </form>
                 @if(session()->has('message'))
@@ -42,17 +42,17 @@
 
     <script>
 
-            document.getElementById('find_client').addEventListener('change', function () {
+        document.getElementById('find_client').addEventListener('change', function () {
             var debitInput = document.getElementById('debit');
             var selectedOption = this.options[this.selectedIndex];
 
             if (selectedOption.value === '0') {
-            debitInput.disabled = true;
-        } else {
-            debitInput.disabled = false;
-        }
+                debitInput.disabled = true;
+            } else {
+                debitInput.disabled = false;
+            }
         });
-            
+
     </script>
 
 @endsection
--- resources/views/showDebtorForm.blade.php
+++ resources/views/showDebtorForm.blade.php
@@ -4,13 +4,14 @@
     <div class="container">
         <div class="row">
             <div class="col-3">
-                <a href="{{ route('home.allDebtors') }}" class="btn btn-primary form-control m-2">Svi klijenti / Dužnici</a>
+                <a href="{{ route('home.allDebtors') }}" class="btn btn-primary form-control m-2">Svi klijenti /
+                    Dužnici</a>
                 <a href="{{ route('homeGlasses') }}" class="btn btn-primary form-control m-2">Svi klijenti</a>
 
             </div>
             <div class="col-1"></div>
             <div class="col-8">
-            <h2>Novi duznik</h2>
+                <h2>Novi dužnik:</h2>
             </div>
         </div>
         <div class="row">
@@ -20,12 +21,14 @@
                     @csrf
                     <label for="find_client">Pronađi klijenta:</label>
                     <select name="client" class="form-control" id="find_client">
+                        <option value="0" id="control">Klikni ovde</option>
                         @foreach($all_clients as $single_client)
                             <option value="{{ $single_client->id}}">{{ $single_client->name }}</option>
                         @endforeach
                     </select><br>
                     <label for="debit">Iznos duga:</label>
-                    <input type="number" name="debit" class="form-control" id="debit" required><br>
+                    <input type="number" name="debit" class="form-control" id="debit"
+                           placeholder="Upis duga je moguć kada odaberete klijenta" disabled required><br>
                     <button class="btn btn-primary form-control">Zapamti</button>
                 </form>
                 @if(session()->has('message'))
@@ -36,6 +39,21 @@
             </div>
         </div>
     </div>
+
+    <script>
+
+        document.getElementById('find_client').addEventListener('change', function () {
+            var debitInput = document.getElementById('debit');
+            var selectedOption = this.options[this.selectedIndex];
+
+            if (selectedOption.value === '0') {
+                debitInput.disabled = true;
+            } else {
+                debitInput.disabled = false;
+            }
+        });
+
+    </script>
 
 @endsection
 
