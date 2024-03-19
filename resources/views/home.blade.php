4f022b0231dd22dd7fbd6482fe177cb2a692b9fd
sinisaglavas <sinisaglavas@ymail.com>
2024-03-19 10:34:56 +0100

    sitne promene

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,10 +26,10 @@
                 <a href="{{ route('showDebtCompanyForm') }}" class="btn btn-info mt-5 p-4" style="width: 100%;"><h2>Obrazac za PIO i druge organizacije</h2></a>
             </div>
             @if($currentUrl == 'https://ns-24.optikaglavas.com/home')
-            <div class="col-7 d-flex flex-column justify-content-center align-items-center" style="height: 580px; background-image: url('{{ asset('images/optikans.jpg') }}'); background-size: cover; background-position: center;">
+            <div class="col-7 d-flex flex-column justify-content-center align-items-center" style="height: 580px; background-image: url('{{ asset('images/okoglavas-ns.jpg') }}'); background-size: cover; background-position: center;">
             </div>
             @endif
-            @if($currentUrl == 'https://bp2024.optikaglavas.com/home')
+            @if($currentUrl == 'http://bp2024.optikaglavas.com/home')
                 <div class="col-7 d-flex flex-column justify-content-center align-items-center" style="height: 580px; background-image: url('{{ asset('images/okoglavasbp.jpg') }}'); background-size: cover; background-position: top;">
                 </div>
             @endif
