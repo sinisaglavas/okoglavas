d387f02243a372654858189b6f04d6db1f0a0d40
sinisaglavas <sinisaglavas@ymail.com>
2025-01-24 15:36:02 +0100

    trenutna godina na home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,18 @@
 {{--        </div>--}}
         <div class="row">
             <div class="col-5">
+                @if($currentUrl == 'https://bp2024.optikaglavas.com/home')
+                    <div class="h2 d-flex flex-column justify-content-center align-items-center">2024. godina</div>
+                @endif
+                @if($currentUrl == 'https://ns2024.optikaglavas.com/home')
+                    <div class="h2 d-flex flex-column justify-content-center align-items-center">2024. godina</div>
+                @endif
+                @if($currentUrl == 'https://bp2025.optikaglavas.com/home')
+                    <div class="h2 d-flex flex-column justify-content-center align-items-center">2025. godina</div>
+                @endif
+                @if($currentUrl == 'https://ns2025.optikaglavas.com/home')
+                        <div class="h2 d-flex flex-column justify-content-center align-items-center">2025. godina</div>
+                @endif
                 <a href="{{ route('homeGlasses') }}" class="btn btn-primary mb-5 p-4" style="width: 100%;"><h2>Klijenti Naočare</h2></a>
                 <br>
                 <a href="{{ route('homeContactLenses') }}" class="btn btn-danger p-4" style="width: 100%;"><h2>Klijenti Kontaktna sočiva</h2></a>
@@ -26,12 +38,16 @@
                 <a href="{{ route('showDebtCompanyForm') }}" class="btn btn-info mt-5 p-4" style="width: 100%;"><h2>Obrazac za PIO i druge organizacije</h2></a>
             </div>
             @if($currentUrl == 'https://ns-24.optikaglavas.com/home')
-            <div class="col-7 d-flex flex-column justify-content-center align-items-center" style="height: 580px; background-image: url('{{ asset('images/okoglavas-ns.jpg') }}'); background-size: cover; background-position: center;">
-            </div>
+            <div class="col-7 d-flex flex-column justify-content-center align-items-center" style="height: 580px; background-image: url('{{ asset('images/okoglavas-ns.jpg') }}'); background-size: cover; background-position: center;"></div>
+            @endif
+            @if($currentUrl == 'https://ns2025.optikaglavas.com/home')
+                <div class="col-7 d-flex flex-column justify-content-center align-items-center" style="height: 580px; background-image: url('{{ asset('images/okoglavas-ns.jpg') }}'); background-size: cover; background-position: center;"></div>
             @endif
             @if($currentUrl == 'https://bp2024.optikaglavas.com/home')
-                <div class="col-7 d-flex flex-column justify-content-center align-items-center" style="height: 580px; background-image: url('{{ asset('images/okoglavasbp.jpg') }}'); background-size: cover; background-position: top;">
-                </div>
+                <div class="col-7 d-flex flex-column justify-content-center align-items-center" style="height: 580px; background-image: url('{{ asset('images/okoglavasbp.jpg') }}'); background-size: cover; background-position: top;"></div>
+            @endif
+            @if($currentUrl == 'https://bp2025.optikaglavas.com/home')
+                <div class="col-7 d-flex flex-column justify-content-center align-items-center" style="height: 580px; background-image: url('{{ asset('images/okoglavasbp.jpg') }}'); background-size: cover; background-position: top;"></div>
             @endif
         </div>
     </div>
